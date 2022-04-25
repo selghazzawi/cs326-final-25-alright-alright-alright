8563774fc45b2a5febf35c47d7a21ec49f814015
selghazzawi <selghazzawi@umass.edu>
2022-04-25 16:38:02 -0400

    Milestone3 push test mongodb code for server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import logger from 'morgan';
 import { readFile, writeFile } from 'fs/promises';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
 
 //'https://jsonplaceholder.typicode.com/todos/1'
 const fakeTrendingData = 'server/fakeTrendingData.json';
 const fakeInterestData = 'server/fakeInterestData.json';
 
 let theGist = {};
-let interest = {}
+let interest = {};
+ 
 
 async function reloadTrending(filename) {
     try {
@@ -200,75 +203,130 @@ async function dumpInterestTopics(response) {
     response.json(interest);
 }
 
-const app = express();
-const port = 3000;
-
-app.use(logger('dev'));
-
-// NEW: Add json and urlencoded middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use('/client', express.static('client'));
-
-//
-
-app.post('/createTrending', async (request, response) => {
-    const options = request.body;
-    createTrendingTopic(response, options.name);
-});
-
-app.post('/createInterest', async (request, response) => {
-    const options = request.body;
-    createInterestTopic(response, options.name);
-});
-
-app.get('/readTrending', async (request, response) => {
-    const options = request.query;
-    readTrendingTopic(response, options.name);
-});
-
-app.get('/readInterest', async (request, response) => {
-    const options = request.query;
-    readInterestTopic(response, options.name);
-});
-
-app.put('/updateTrending', async (request, response) => {
-    const options = request.query;
-    updateTrendingTopic(response, options.name, options.analysis);
-});
-
-app.put('/updateInterest', async (request, response) => {
-    const options = request.query;
-    updateInterestTopic(response, options.name, options.analysis);
-});
-
-app.delete('/deleteTrending', async (request, response) => {
-    const options = request.query;
-    deleteTrendingTopic(response, options.name);
-});
-
-app.delete('/deleteInterest', async (request, response) => {
-    const options = request.query;
-    deleteInterestTopic(response, options.name);
-});
-
-
-app.get('/dumpTrending', async (request, response) => {
-    const options = request.body;
-    dumpTrendingTopics(response);
-});
-
-app.get('/dumpInterest', async (request, response) => {
-    const options = request.body;
-    dumpInterestTopics(response);
-});
 
 /*app.get('*', async (request, response) => {
     response.status(404).send(`Not found: ${request.path}`);
 });*/
+//const uri = process.env.MONGODB_URI;
+//const client = new MongoClient(uri);
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-  });
-  
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+    
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`)); 
+}
+
+/*async function createCollection(client) {
+    client.createCollection("testRun", function(err, result) {
+        if (err) throw err;
+        console.log("Collection is created!");
+        // close the connection to db when you are done with it
+
+    });
+}*/
+
+
+
+async function main() {
+    const app = express();
+    const port = 3000;
+    
+    app.use(logger('dev'));
+    
+    // NEW: Add json and urlencoded middleware
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    
+    app.use('/client', express.static('client'));
+    
+    //
+    
+    app.post('/createTrending', async (request, response) => {
+        const options = request.body;
+        createTrendingTopic(response, options.name);
+    });
+    
+    app.post('/createInterest', async (request, response) => {
+        const options = request.body;
+        createInterestTopic(response, options.name);
+    });
+    
+    app.get('/readTrending', async (request, response) => {
+        const options = request.query;
+        readTrendingTopic(response, options.name);
+    });
+    
+    app.get('/readInterest', async (request, response) => {
+        const options = request.query;
+        readInterestTopic(response, options.name);
+    });
+    
+    app.put('/updateTrending', async (request, response) => {
+        const options = request.query;
+        updateTrendingTopic(response, options.name, options.analysis);
+    });
+    
+    app.put('/updateInterest', async (request, response) => {
+        const options = request.query;
+        updateInterestTopic(response, options.name, options.analysis);
+    });
+    
+    app.delete('/deleteTrending', async (request, response) => {
+        const options = request.query;
+        deleteTrendingTopic(response, options.name);
+    });
+    
+    app.delete('/deleteInterest', async (request, response) => {
+        const options = request.query;
+        deleteInterestTopic(response, options.name);
+    });
+    
+    
+    app.get('/dumpTrending', async (request, response) => {
+        const options = request.body;
+        dumpTrendingTopics(response);
+    });
+    
+    app.get('/dumpInterest', async (request, response) => {
+        const options = request.body;
+        dumpInterestTopics(response);
+    });
+
+    const uri = process.env.MONGODB_URI;
+    console.log(uri)
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        const dbo = client.db('mydb');
+        console.log(dbo);
+        const curCollection = dbo.collection('temp');
+        console.log(curCollection);
+        curCollection.insertOne( { item: "card", qty: 15 } );
+        /*dbo.createCollection('temp', function(err, res) {
+            if (err) throw err;
+            console.log("Collection created!");
+            dbo.close();
+          });*/
+        /*client.db.createCollection("temp", function(err, res) {
+            if (err) throw err;
+            console.log("Collection created!");
+            db.close();
+          });*/
+        await  listDatabases(client);
+        //await createCollection(client);
+        //await listDatabases(client);
+    }
+    catch (e) {
+        console.error(e);
+    }
+    finally {
+        await client.close();
+    }
+
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+      });
+
+}
+main().catch(console.error);
